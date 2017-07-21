0464863bc252cf3611d21a35f4418fbf6fdd8aae
K J Venkatesh <kjvenky@gmail.com>
2017-07-21 17:38:39 +0530

    Fixed the websocket connection and added mqtt.html

--- NinjaFrontEnd/js/mqtt.js
+++ NinjaFrontEnd/js/mqtt.js
@@ -1,7 +1,8 @@
+"use strict"
+
 // Configure Cognito identity pool
-AWS.config.region = 'us-east-1';
 var credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:',
+    IdentityPoolId: 'us-west-2:7b7923c1-68de-4a32-8fe2-52840b399eb9',
 });
 
 //Getting AWS creds from Cognito is async, so we need to drive the rest of the mqtt client initialization in a callback
@@ -10,14 +11,15 @@ credentials.get(function(err) {
         console.log(err);
         return;
     }
-    var requestUrl = SigV4Utils.getSignedUrl('wss', 'data.iot.us-east-1.amazonaws.com', '/mqtt',
-        'iotdevicegateway', 'us-east-1',
+    var requestUrl = SigV4Utils.getSignedUrl('wss', 'data.iot.us-west-2.amazonaws.com', '/mqtt',
+        'iotdevicegateway', 'us-west-2',
         credentials.accessKeyId, credentials.secretAccessKey, credentials.sessionToken);
+    console.log('requestUrl:', requestUrl)
     initClient(requestUrl);
 });
 
 function init() {
-  // do setup stuff
+    // console.log("Initializing the function")
 }
 
 // Connect the client, subscribe to the drawing topic, and publish a "hey I connected" message
@@ -29,25 +31,27 @@ function initClient(requestUrl) {
             console.log('connected');
 
             // subscribe to the drawing
-            client.subscribe("your/mqtt/topic");
+            client.subscribe("aws/things/test-iot/shadow");
 
             // publish a lifecycle event
             message = new Paho.MQTT.Message('{"id":"' + credentials.identityId + '"}');
-            message.destinationName = 'your/mqtt/topic';
+            message.destinationName = 'aws/things/test-iot/shadow';
+            console.log("I am here");
             console.log(message);
             client.send(message);
         },
         useSSL: true,
         timeout: 3,
         mqttVersion: 4,
-        onFailure: function () {
+        onFailure: function (err) {
             console.error('connect failed');
+            console.error(err);
         }
     };
+
     client.connect(connectOptions);
 
     client.onMessageArrived = function (message) {
-
         try {
             console.log("msg arrived: " +  message.payloadString);
         } catch (e) {
